ee0e937a9e4a0abe1c5e16611748cd172f36bdb7
Mahad Toukaleh <mahad.toukaleh56@gmail.com>
2025-05-27 18:20:50 -0400

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
         <meta name="theme-color" content="#ffffff" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </head>
